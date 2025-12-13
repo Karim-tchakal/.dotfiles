18cd7e334d8eef8584cf8751d9348c552e7123e1
Karim Tchakal <karrimtchakal@gmail.com>
2025-12-13 14:16:55 +0100

    feat: update theme colors and enhance Waybar configuration with tooltips

--- scripts/scripts/toggle_theme.sh
+++ scripts/scripts/toggle_theme.sh
@@ -11,12 +11,17 @@ if [[ "$CURRENT" == "$DARK_DIR/dark.css" ]]; then
     ln -sf "$LIGHT_DIR/vscode_light.json" "$THEME_DIR/vscode.json"
     ln -sf "$LIGHT_DIR/ascii_light.txt" "$HOME/.dotfiles/fastfetch/fastfetch/ascii.txt"
     ln -sf "$LIGHT_DIR/hyprland_light.conf" "$THEME_DIR/hyprland.conf"
+    touch "$THEME_DIR/light.mode"
 else
+
     ln -sf "$DARK_DIR/dark.css" "$THEME_DIR/current_colors.css"
     ln -sf "$DARK_DIR/ghostty_dark.conf" "$THEME_DIR/ghostty.conf"
     ln -sf "$DARK_DIR/vscode_dark.json" "$THEME_DIR/vscode.json"
     ln -sf "$DARK_DIR/ascii_dark.txt" "$HOME/.dotfiles/fastfetch/fastfetch/ascii.txt"
     ln -sf "$DARK_DIR/hyprland_dark.conf" "$THEME_DIR/hyprland.conf"
+    if [ -f "$THEME_DIR/light.mode" ]; then
+        rm "$THEME_DIR/light.mode"
+    fi
 fi
 
 # Reload Waybar to pick up new colors
@@ -25,4 +30,4 @@ pkill -SIGUSR2 -x ghostty
 sed -i '' /home/karim/.dotfiles/waybar/waybar/style.css
 theme_name=$(jq -r '.name' ~/.config/omarchy/current/theme/vscode.json)
 sed -i -E "s/(\"workbench.colorTheme\"[[:space:]]*:[[:space:]]*\")[^\"]*(\")/\1$theme_name\2/" ~/.config/Code/User/settings.json
-
+omarchy-theme-set-gnome
